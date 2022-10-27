1361fdbbdc81c34e19503cab6c253e49b4e1ffd0
Sarah079 <u19032618@tuks.co.za>
2022-10-27 22:23:44 +0200

    Add project files.

---
+++ Homework 6/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Homework_6.Models;
+using PagedList;
+
+namespace Homework_6.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BikeStoresEntities db = new BikeStoresEntities();
+
+        public ActionResult Orders()
+        {
+            string inputdate = "";
+            var orders = db.orders.Include(o => o.order_date);
+            
+            return View();
+        }
+    }
+}
+//            var products = db.products.Include(p => p.brand).Include(p => p.category);
+//                products = products.Where(s => s.product_name.Contains(searchString));
---
+++ Homework 6/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Homework_6.MvcApplication" Language="C#" %>
